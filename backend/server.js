6e7cf98ca9ee5c167566a78473ece9e0f91f6774
raselsiu <raselqax@gmail.com>
2023-09-14 01:55:07 +0600

    Mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const color = require('colors')
 const goalRoutes = require('./routes/goalRoutes')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const port  = process.env.PORT||5000
 const app = express()
@@ -10,6 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+connectDB()
 
 app.use('/api/goals/', goalRoutes);
 
