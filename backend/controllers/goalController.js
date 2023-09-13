1efa290b44558759c980b53a6d8a44176dc6a8c1
raselsiu <raselqax@gmail.com>
2023-09-13 23:57:21 +0600

    middleware added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,37 @@
+const asyncHandler = require('express-async-handler')
+
+
 //@des    GET goals
 //@route  GET /api/goals
 //@access Private
-const getGoals = (req,res) =>{
+const getGoals = asyncHandler(async(req,res) =>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message:'get goals'})
-}
+})
 
 //@des    SET goals
 //@route  SET /api/goals
 //@access Private
-const setGoals = (req,res) =>{
+const setGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message:'set goals'})
-}
+})
 
 //@des    UPDATE goals
 //@route  UPDATE /api/goals/:ID
 //@access Private
-const updateGoals = (req,res) =>{
+const updateGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message:`update goals ${req.params.id}`})
-}
+})
 
 //@des    DELETE goals
 //@route  DELETE /api/goals/:ID
 //@access Private
-const deleteGoals = (req,res) =>{
+const deleteGoals = asyncHandler(async(req,res) =>{
     res.status(200).json({message:`deleted goals ${req.params.id}`})
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const goalRoutes = require('./routes/goalRoutes')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const port  = process.env.PORT||5000
 const app = express()
 
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
 app.use('/api/goals/', goalRoutes);
+
+app.use(errorHandler)
 
 
 app.listen(port,()=>{
